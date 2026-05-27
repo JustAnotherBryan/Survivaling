e02ab9af9c06cec34d9499ba373bb8ebf905b463
JustAnotherBryan <yeezhengxun@gmail.com>
2026-05-28 02:35:27 +0800

    Setting up proper health
    Setting up health,
    pressing left click damages10
    pressing right click heals10
    
    Bugs rn is that the health goes 0 to 100 immediately

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,6 +1,69 @@
 using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class PlayerHealth
+public class PlayerHealth : MonoBehaviour
 {
-    
+    public int maxHealth = 100;
+    public int currentHealth;
+    [SerializeField] private Slider healthSlider;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        
+        // Initialize slider min and max values
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+        }
+    }
+
+
+    // Call this method whenever the player takes damage
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        UpdateHealthUI();
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        UpdateHealthUI();
+    }
+
+    private void UpdateHealthUI()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth; // Bind health to slider value
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("The left mouse button is being held down.");
+            TakeDamage(10);
+            Debug.Log(currentHealth);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("The right mouse button is being held down.");
+            Heal(10);
+            Debug.Log(currentHealth);
+        }
+    } 
+
 }
